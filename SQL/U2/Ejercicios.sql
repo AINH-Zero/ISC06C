47e54ddc7fd21af65bad2f3c3d1b8c72f6fcd869
AINH-Zero <issac.nun2@outlook.es>
2025-10-20 20:10:30 -0600

    Case

--- SQL/U2/Ejercicios.sql
+++ SQL/U2/Ejercicios.sql
@@ -7,4 +7,12 @@ GO
 --FROM Libros L
 --JOIN Autores A ON L.AutorID = A.AutorID
 --WHERE A.Nacionalidad = 'Colombiana';
-SELECT * FROM vw_LibrosAutores_Colombianos;
+--SELECT * FROM vw_LibrosAutores_Colombianos;
+
+-- Ejercicio 2
+--CREATE VIEW vw_Libros_Disponiblie
+--AS
+--SELECT Titulo, CASE WHEN Disponible = 0 THEN 'Prestado :o' ELSE 'Disponible :)'
+--  END AS estado_del_campo
+--FROM Libros;
+SELECT * FROM vw_Libros_Disponiblie;
